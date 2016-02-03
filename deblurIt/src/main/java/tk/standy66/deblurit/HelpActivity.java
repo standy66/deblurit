2da882f1a6d7c6755cfd301e3999487e7ee521fa
Andrew Stepanov <karn9050@gmail.com>
2016-02-03 19:57:15 +0300

    Simplify layouts

--- deblurIt/src/main/java/tk/standy66/deblurit/HelpActivity.java
+++ deblurIt/src/main/java/tk/standy66/deblurit/HelpActivity.java
@@ -13,18 +13,11 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         TextView main = (TextView)findViewById(R.id.help_textview);
-        main.setText(Html.fromHtml(getResources().getString(R.string.help_message), new Html.ImageGetter() {			
+        main.setText(Html.fromHtml(getResources().getString(R.string.help_message), new Html.ImageGetter() {
             public Drawable getDrawable(String source) {
                 Drawable drawable = getResources().getDrawable(R.drawable.circle);
                 drawable.setBounds(0, 0, 50, 50);
                 return drawable;
-                /*int id = 0;
-                if (source.equals("circle"))
-                    id = R.drawable.circle;
-                if (id == 0)
-                    return null;
-                Drawable d = getResources().getDrawable(id);
-                return d;*/
             }
         }, null));
     }
